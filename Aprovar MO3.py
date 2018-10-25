f5d71beabcc4bf9517a8770a19b7ed11c41bd85e
DRAVEN14 <44439459+DRAVEN14@users.noreply.github.com>
2018-10-25 11:43:42 +0200

    Update Aprovar MO3.py

--- Aprovar MO3.py
+++ Aprovar MO3.py
@@ -1,8 +1,8 @@
 # Como aprovar M03
-jamon = input("Has comprado jamon?")
-estudiar = input("Cuanto has estudiado?")
-Abducion = input("Te han abducido")
-dia = input("Cuando te han abducido")
+jamon = input("Has comprado jamon? ")
+estudiar = input("Cuanto has estudiado? ")
+Abducion = input("Te han abducido ")
+dia = input("Cuando te han abducido ")
 if (jamon == "si")or(estudiar == "mucho")or((Abducion == "si")and(dia == "J")):
    print("Felicidades has aprovado M03")
 else:
