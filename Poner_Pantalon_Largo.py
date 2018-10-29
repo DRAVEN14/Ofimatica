29071d678efb1c95e3f211d4b42fe5042197efca
DRAVEN14 <44439459+DRAVEN14@users.noreply.github.com>
2018-10-29 18:57:10 +0100

    Update Poner_Pantalon_Largo.py

--- Poner_Pantalon_Largo.py
+++ Poner_Pantalon_Largo.py
@@ -2,6 +2,6 @@ estacion =input("En que estacion estamos: ")
 hora =int(input("Que hora es: ")
 temperatura =int(input("Que temperatura estamos hoy: ")
 
-if (estacion = "verano")and((hora >= 22)and(hora <= 23:59)and(hora >= 0)and(hora <= 4))and(temperatura <= 15):
+if (estacion = "verano")and(hora >= 22)and(hora <= 23:59)and(hora >= 0)and(hora <= 4)and(temperatura <= 15):
     print("Ponte pantalon largo ")
     
