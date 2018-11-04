ce8878602ea3f737d6cd0976fa66429ea43a143c
DRAVEN14 <44439459+DRAVEN14@users.noreply.github.com>
2018-11-04 15:42:11 +0100

    Add files via upload

---
+++ Ejerecicio edad 3.py
@@ -0,0 +1,6 @@
+edad = int(input("Introduca su edad: "))
+
+if (edad >= 18) and (edad <= 23) or (edad == 17):
+    print( "Puedes entrar en la sesion de jovenes" )
+else:
+    print( "No puedes entrar en la sesion de jovenes" )
