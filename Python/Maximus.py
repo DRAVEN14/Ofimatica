8e2371ced308d629e026be1a7a839b5b74701fc2
DRAVEN14 <44439459+DRAVEN14@users.noreply.github.com>
2018-11-15 10:18:48 +0100

    Create Maximus.py

---
+++ Python/Maximus.py
@@ -0,0 +1,10 @@
+numero1 = int(input("Introdueix el primer: "))
+numero2 = int(input("Introdueix el segon: "))
+if (numero1==numero2):
+    print("Son iguales")
+else:
+    if(numero1>numero2):
+        print("El mes gran es",numero1)
+    else:
+        if(numero1<numero2):
+            print("El mes gran es",numero2)
