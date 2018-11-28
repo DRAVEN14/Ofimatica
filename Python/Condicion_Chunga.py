a78a1ef11669f44eb17ed99d85cb9b7282556ff5
DRAVEN14 <44439459+DRAVEN14@users.noreply.github.com>
2018-11-28 12:58:00 +0100

    Create Condicion_Chunga.py

---
+++ Python/Condicion_Chunga.py
@@ -0,0 +1,13 @@
+numero = int(input("Introduce cualquier numero: "))
+if(numero%2==0):
+    print("Es par")
+else:
+    print("Es impar")
+if(numero<=40 and numero>=-10):
+    print("Esta entre -10 y 40")
+else:
+    print("No esta entre -10 y 40")
+if(numero<0):
+    print("Es negativo")
+else:
+    print("No es negativo")
