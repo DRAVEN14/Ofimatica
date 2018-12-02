895ba7d681f4881074f8bbd015b257060b36b7b1
DRAVEN14 <44439459+DRAVEN14@users.noreply.github.com>
2018-12-02 18:37:16 +0100

    Create Gasolinera.py

---
+++ Python/Gasolinera.py
@@ -0,0 +1,5 @@
+gasolina = input("Super:Normal1 (1,5€), Turbo (1,55€) " "Sin plomo:Normal2 (1,6€), Con aditivos(sabor naranja) (1,65€) " "Diesel:Normal3 (1,7€), Fast&Furius (1,75€): ")
+litro = float(input("Cuantos litros quieres: "))
+importe = float(litro*
+if (gasolina=="Normal1"or gasolina=="Turbo" or gasolina=="Normal2" or gasolina=="Con aditivos" or gasolina=="Normal3" or gasolina=="Fast&Furius"):
+    
