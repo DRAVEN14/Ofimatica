b02fc7b07730f91c4341a51788945afcce88f5c9
DRAVEN14 <44439459+DRAVEN14@users.noreply.github.com>
2018-12-03 10:18:05 +0100

    Update Gasolinera.py

--- Python/Gasolinera.py
+++ Python/Gasolinera.py
@@ -1,5 +1,37 @@
-gasolina = input("Super:Normal1 (1,5€), Turbo (1,55€) " "Sin plomo:Normal2 (1,6€), Con aditivos(sabor naranja) (1,65€) " "Diesel:Normal3 (1,7€), Fast&Furius (1,75€): ")
-litro = float(input("Cuantos litros quieres: "))
-importe = float(litro*
-if (gasolina=="Normal1"or gasolina=="Turbo" or gasolina=="Normal2" or gasolina=="Con aditivos" or gasolina=="Normal3" or gasolina=="Fast&Furius"):
+print("Menu de Gasolina")
+print("Super:Normal (1,5€), Turbo (1,55€)")
+print("Sin plomo:Normal (1,6€), Con aditivos(sabor naranja) (1,65€)")
+print("Diesel:Normal (1,7€), Fast&Furius (1,75€)")
+super_normal=1
+super_turbo=2
+sin_plomo_normal=3
+sin_plomo_con_aditivos=4
+diesel_normal=5
+diesel_fast_furius=6
+gasolina=int(input("Elige que tipo de gasolina quieres: "))
+litros=int(input("Cuantos litros quieres: "))
+if(gasolina==1):
+    res_1=(litros*1.5)
+    print("Total a pagar",res_1,"€")
+else:
+    if(gasolina==2):
+        res_2=(litros*1.55)
+        print("Total a pagar",res_2,"€")
+    else:
+        if(gasolina==3):
+            res_3=(litros*1.6)
+            print("Total a pagar",res_3,"€")
+        else:
+            if(gasolina==4):
+                res_4=(litros*1.65)
+                print("Total a pagar",res_4,"€")
+            else:
+                if(gasolina==5):
+                    res_5=(litros*1.7)
+                    print("Total a pagar",res_5,"€")
+                else:
+                    if(gasolina==6):
+                        res_6=(litros*1.75)
+                        print("Total a pagar",res_6,"€")
+                        
     
